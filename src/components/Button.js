535b7238563c300f6112502aa3ae6b64105d83ae
Artem Aleksikov <fizrukaleksikov@gmail.com>
2023-04-30 16:56:36 +0300

    feat: add activation of caps lock

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,11 +3,16 @@ import { State } from "../common/State";
 
 export class Button extends HTMLElement {
   constructor(parentNode, key, keyboardUpdate) {
-    super(parentNode, "button", "button", key.en);
+    super(parentNode, "button", "button", key.sign);
     this.key = key;
     this.state = new State();
     this.state.onUpdate.subscribe(keyboardUpdate);
 
-    this.node.onclick = () => this.state.setData(this.key);
+    this.node.onclick = () => {
+      if (this.key.sign === "Caps Lock") {
+        this.node.classList.toggle("capslock-active");
+      }
+      this.state.setData(this.key);
+    };
   }
 }
