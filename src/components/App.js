33f950aac9c9e43e0c613c703b4cd02dccd78a90
Artem Aleksikov <fizrukaleksikov@gmail.com>
2023-04-30 17:00:25 +0300

    refactor: moved the event handler to a lower level

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,7 @@ export class App extends HTMLElement {
   constructor(parentNode, keys) {
     super(parentNode, "main", "main");
 
-    const output = new Output(this.node);
+    const output = new Output(this.node, keys);
     new Keyboard(this.node, keys, output.update.bind(output));
-    window.addEventListener("keydown", (e) => {
-      if (document.activeElement.tagName === "TEXTAREA") {
-        output.history.push(e.key);
-      }
-    });
   }
 }
