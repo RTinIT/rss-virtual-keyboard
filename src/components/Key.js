cd135592bd244133cc81ef0ef149a3e3674a7a4f
Artem Aleksikov <fizrukaleksikov@gmail.com>
2023-05-04 01:51:06 +0300

    feat: add classes for split keys

---
+++ src/components/Key.js
@@ -0,0 +1,21 @@
+import { hasCapsLock } from "./KeyCapsLock";
+
+export class Key {
+  constructor(data, dataKeys, cases, cursorPlace) {
+    this.data = data;
+    this.cases = cases;
+    this.dataKeys = dataKeys;
+    this.cursorPlace = cursorPlace;
+  }
+
+  action(code, event) {
+    const { shift, sign } = this.dataKeys[code] ? this.dataKeys[code] : "";
+    if (event?.shiftKey && event?.key !== "Shift") {
+      this.data.add(hasCapsLock.getValue() ? sign : shift);
+      return this.cursorPlace++;
+    }
+    if (event?.key === "Shift") return this.cursorPlace;
+    this.data.add(hasCapsLock.getValue() ? shift : sign);
+    return this.cursorPlace++;
+  }
+}
