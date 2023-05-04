5ed47112fe9e8f98a5e73ff74ec89b53d7d7a828
Artem Aleksikov <fizrukaleksikov@gmail.com>
2023-05-04 16:13:35 +0300

    refactor: add space

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,6 +4,7 @@ import { State } from "../common/State";
 export class Button extends HTMLElement {
   constructor(parentNode, key, keyboardUpdate) {
     super(parentNode, "button", "button");
+
     this.key = key;
     this.state = new State();
     this.state.onUpdate.subscribe(keyboardUpdate);
@@ -20,6 +21,7 @@ export class Button extends HTMLElement {
         ? ""
         : this.key.shift
     );
+
     this.mainKey = new HTMLElement(
       this.node,
       "span",
